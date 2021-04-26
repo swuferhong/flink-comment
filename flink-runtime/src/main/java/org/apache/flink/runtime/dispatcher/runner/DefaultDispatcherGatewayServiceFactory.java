89d5b3905bc09573381e359e38d5b84b50b15670
zhengyunhong <337361684@qq.com>
2021-04-26 20:22:31 +0800

    My flink source code comment-first comment

---
+++ flink-runtime/src/main/java/org/apache/flink/runtime/dispatcher/runner/DefaultDispatcherGatewayServiceFactory.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.runtime.dispatcher.runner;
+
+import org.apache.flink.runtime.dispatcher.Dispatcher;
+import org.apache.flink.runtime.dispatcher.DispatcherFactory;
+import org.apache.flink.runtime.dispatcher.DispatcherId;
+import org.apache.flink.runtime.dispatcher.NoOpDispatcherBootstrap;
+import org.apache.flink.runtime.dispatcher.PartialDispatcherServices;
+import org.apache.flink.runtime.dispatcher.PartialDispatcherServicesWithJobGraphStore;
+import org.apache.flink.runtime.jobgraph.JobGraph;
+import org.apache.flink.runtime.jobmanager.JobGraphWriter;
+import org.apache.flink.runtime.rpc.RpcService;
+import org.apache.flink.util.FlinkRuntimeException;
+
+import java.util.Collection;
+
+/**
+ * Factory for the {@link DefaultDispatcherGatewayService}.
+ */
+class DefaultDispatcherGatewayServiceFactory implements AbstractDispatcherLeaderProcess.DispatcherGatewayServiceFactory {
+
+	private final DispatcherFactory dispatcherFactory;
+
+	private final RpcService rpcService;
+
+	private final PartialDispatcherServices partialDispatcherServices;
+
+	DefaultDispatcherGatewayServiceFactory(
+			DispatcherFactory dispatcherFactory,
+			RpcService rpcService,
+			PartialDispatcherServices partialDispatcherServices) {
+		this.dispatcherFactory = dispatcherFactory;
+		this.rpcService = rpcService;
+		this.partialDispatcherServices = partialDispatcherServices;
+	}
+
+	@Override
+	public AbstractDispatcherLeaderProcess.DispatcherGatewayService create(
+			DispatcherId fencingToken,
+			Collection<JobGraph> recoveredJobs,
+			JobGraphWriter jobGraphWriter) {
+
+		final Dispatcher dispatcher;
+		try {
+			/*TODO 真正的Dispatcher创建的地方*/
+			dispatcher = dispatcherFactory.createDispatcher(
+				rpcService,
+				fencingToken,
+				recoveredJobs,
+				(dispatcherGateway, scheduledExecutor, errorHandler) -> new NoOpDispatcherBootstrap(),
+				PartialDispatcherServicesWithJobGraphStore.from(partialDispatcherServices, jobGraphWriter));
+		} catch (Exception e) {
+			throw new FlinkRuntimeException("Could not create the Dispatcher rpc endpoint.", e);
+		}
+
+		/*TODO 开始启动dispatcher， dispatcher是flink jobManager的一个组件*/
+		/*TODO dispatcher的两个主要的任务： 1.接收用户的作业。 2.启动JobMaster*/
+		dispatcher.start();
+		/*TODO  因为Flink的通信是基于Akka的，所以当这里调用start()的时候，对应的就会调用onStart()方法*/
+
+		return DefaultDispatcherGatewayService.from(dispatcher);
+	}
+}
